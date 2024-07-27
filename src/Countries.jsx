1bdea66e46c38a02bff5c29cd1c6ee8391f4ddda
sajidhakhan <81672634+sajidhakhan@users.noreply.github.com>
2024-07-27 16:03:38 +0530

    test case

--- src/Countries.jsx
+++ src/Countries.jsx
@@ -1,58 +1,51 @@
 import React, { useEffect, useState } from 'react';
 import styles from './Countries.module.css';
 
- function Countries() {
-  
+function Countries() {
     const apiUrl = "https://restcountries.com/v3.1/all";
-  
-    const [countries, setCountries] = useState ([]);
 
-    const [isLoading,setIsLoading] = useState ([false]);
+    const [countries, setCountries] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setIsError] = useState(null);
 
-    const [error, setIsError] = useState ([null]);
-
-
-    useEffect(()=>{
+    useEffect(() => {
         const fetchData = async () => {
             setIsLoading(true);
             setIsError(null);
-    
+
             try {
                 const response = await fetch(apiUrl);
 
-                if(!response.ok){
-                    throw new Error(`Api request failed with status: ${response.status}`)
+                if (!response.ok) {
+                    throw new Error(`API request failed with status: ${response.status}`);
                 }
                 const data = await response.json();
 
-               setCountries(data);
-
+                setCountries(data);
             } catch (error) {
                 setIsError(error);
-                console.error('Error fetching in countries:', error);
+                console.error('Error fetching countries:', error);
             } finally {
                 setIsLoading(false);
             }
         };
         fetchData();
-    },[]);
-  
+    }, []);
 
-  return (
-    <>
-        {isLoading && <p>Loading countries...</p>}
-        {error && <p>Error: {error.message}</p>}
-       <div className={styles.wrapper} >
-            {!isLoading && !error && countries.map((country) => (             
+    return (
+        <>
+            {isLoading && <p>Loading countries...</p>}
+            {error && <p>Error: {error.message}</p>}
+            <div className={styles.wrapper}>
+                {!isLoading && !error && countries.map((country) => (
                     <div className={styles.countryCard} key={country.cca3}>
                         <img src={country.flags.png} alt={country.name.common} />
                         <h2>{country.name.common}</h2>
-                    </div>               
-            ))}
-        </div>
-    </>
-  );
+                    </div>
+                ))}
+            </div>
+        </>
+    );
 }
 
-
-export default Countries
+export default Countries;

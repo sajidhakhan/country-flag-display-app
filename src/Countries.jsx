c98cf077c371c9cd7a968bbe3526d9864b2abb73
sajidhakhan <81672634+sajidhakhan@users.noreply.github.com>
2024-07-28 15:14:25 +0530

    ---

--- src/Countries.jsx
+++ src/Countries.jsx
@@ -2,28 +2,40 @@ import React, { useEffect, useState } from 'react';
 import styles from './Countries.module.css';
 
 function Countries() {
-
     const apiUrl = "https://restcountries.com/v3.1/all";
-
     const [countries, setCountries] = useState([]);
-    
+    const [error, setError] = useState(null);
 
     useEffect(() => {
         fetch(apiUrl)
-        .then((response) => response.json())
-        .then((data) => setCountries(data));
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+            .then((data) => setCountries(data))
+            .catch((error) => {
+                console.error('Error fetching the data:', error);
+                setError(error);
+            });
     }, []);
 
     return (
         <>
             <div className={styles.wrapper}>
-                
-                { countries.map((country) => (
-                    <div className={styles.countryCard} key={country.cca3}>
-                        <img src={country.flags.png} alt={`${country.name.common} flag`} />
-                        <h2>{country.name.common}</h2>
+                {error ? (
+                    <div className={styles.error}>
+                        <p>Failed to load countries data. Please try again later.</p>
                     </div>
-                ))}
+                ) : (
+                    countries.map((country) => (
+                        <div className={styles.countryCard} key={country.cca3}>
+                            <img src={country.flags.png} alt={`${country.name.common} flag`} />
+                            <h2>{country.name.common}</h2>
+                        </div>
+                    ))
+                )}
             </div>
         </>
     );

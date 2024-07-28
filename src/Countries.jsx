fb1bd8458281b3935c3ad421d902e37a94602fce
sajidhakhan <81672634+sajidhakhan@users.noreply.github.com>
2024-07-28 12:51:17 +0530

    test case fix

--- src/Countries.jsx
+++ src/Countries.jsx
@@ -2,39 +2,22 @@ import React, { useEffect, useState } from 'react';
 import styles from './Countries.module.css';
 
 function Countries() {
+
     const apiUrl = "https://restcountries.com/v3.1/all";
 
     const [countries, setCountries] = useState([]);
-    const [isLoading, setIsLoading] = useState(false);
-    const [error, setIsError] = useState(null);
+    
 
     useEffect(() => {
-        const fetchData = async () => {
-            setIsLoading(true);
-            setIsError(null);
-
-            try {
-                const response = await fetch(apiUrl);
-                if (!response.ok) {
-                    throw new Error(`API request failed with status: ${response.status}`);
-                }
-                const data = await response.json();
-                setCountries(data);
-            } catch (error) {
-                setIsError(error);
-            } finally {
-                setIsLoading(false);
-            }
-        };
-        fetchData();
+        fetch(apiUrl)
+        .then((response) => response.json())
+        .then((data) => setCountries(data));
     }, []);
 
     return (
         <>
-            {isLoading && <p>Loading countries...</p>}
-            {error && <p>Error: {error.message}</p>}
             <div className={styles.wrapper}>
-                {!isLoading && !error && countries.map((country) => (
+                { countries.map((country) => (
                     <div className={styles.countryCard} key={country.cca3}>
                         <img src={country.flags.png} alt={`${country.name.common} flag`} />
                         <h2>{country.name.common}</h2>

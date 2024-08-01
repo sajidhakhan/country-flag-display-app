d0f3ecce1ca8e996f3ce4cb6ea9e474385429697
sajidhakhan <81672634+sajidhakhan@users.noreply.github.com>
2024-08-01 15:28:52 +0530

    fixed errors

--- src/Countries.jsx
+++ src/Countries.jsx
@@ -2,40 +2,46 @@ import React, { useEffect, useState } from 'react';
 import styles from './Countries.module.css';
 
 function Countries() {
+
     const apiUrl = "https://restcountries.com/v3.1/all";
+
     const [countries, setCountries] = useState([]);
-    const [error, setError] = useState(null);
+    
+    const [searchTerm, setSearchTerm] = useState('');
 
     useEffect(() => {
         fetch(apiUrl)
-            .then((response) => {
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
-                return response.json();
-            })
+            .then((response) => response.json())
             .then((data) => setCountries(data))
-            .catch((error) => {
-                console.error('Error fetching the data:', error);
-                setError(error);
-            });
+            .catch((error) => console.error('Error fetching data: ', error));
     }, []);
+
+    const filteredCountries = countries.filter((country) =>
+        country.name.common.toLowerCase().includes(searchTerm.toLowerCase())
+      );
 
     return (
         <>
-            <div className={styles.wrapper}>
-                {error ? (
-                    <div className={styles.error}>
-                        <p>Failed to load countries data. Please try again later.</p>
+         <div className={styles.wrapper}>
+          <div className={styles.headerStyle}>
+                <input
+                type="text"
+                placeholder="Search for countries..."
+                className={styles.searchStyle}
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
+                style={{ width: '400px', height: '40px', fontSize: '16px', padding: '5px 10px' }}
+                
+                />
+            </div>
+            <div className={styles.countryContainer}>
+                { filteredCountries.map((country) => (
+                    <div className={styles.countryCard} key={country.cca3}>
+                        <img src={country.flags.png} alt={`${country.name.common} flag`} />
+                        <h2>{country.name.common}</h2>
                     </div>
-                ) : (
-                    countries.map((country) => (
-                        <div className={styles.countryCard} key={country.cca3}>
-                            <img src={country.flags.png} alt={`${country.name.common} flag`} />
-                            <h2>{country.name.common}</h2>
-                        </div>
-                    ))
-                )}
+                ))}
+            </div>
             </div>
         </>
     );

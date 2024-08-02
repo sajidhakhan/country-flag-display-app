f8b1e5d3f21c1c61ea7c074e6f40e30e69b4542f
sajidhakhan <81672634+sajidhakhan@users.noreply.github.com>
2024-08-02 18:23:29 +0530

    fix test case

--- src/Countries.jsx
+++ src/Countries.jsx
@@ -4,11 +4,11 @@ import styles from './Countries.module.css';
 function Countries() {
 
       const [apiData, setApiData] = useState(null);
-  const [searchTerm, setSearchTerm] = useState("");
+      const [searchTerm, setSearchTerm] = useState("");
 
   const fetchData = async () => {
     try {
-      const response = await fetch("https://restcountries.com/v3.1/all");
+      const response = await fetch("https://xcountries-backend.azurewebsites.net/all");
       const data = await response.json();
       setApiData(data);
     } catch (error) {
@@ -21,10 +21,11 @@ function Countries() {
   }, []);
 
   const filteredCountries = apiData
-    ? apiData.filter((country) =>
-        country.name.common.toLowerCase().includes(searchTerm.toLowerCase())
-      )
-    : [];
+  ? apiData.filter((country) => {
+      const countryName = country.name?.common || country.name || '';
+      return countryName.toLowerCase().includes(searchTerm.toLowerCase());
+    })
+  : [];
 
     return (
         <>
@@ -43,8 +44,8 @@ function Countries() {
             <div className={styles.countryContainer}>
                 { filteredCountries.map((country) => (
                     <div className={styles.countryCard} key={country.cca3}>
-                        <img src={country.flags.png} alt={`${country.name.common} flag`} />
-                        <h2>{country.name.common}</h2>
+                        <img src={country.flag} alt={`${country.name} flag`} />
+                        <h2>{country.name}</h2>
                     </div>
                 ))}
             </div>
